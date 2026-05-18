0e617d93095fc4457503bb69645f988c4e24912d
Ishaan <ishaanshah21@gmail.com>
2026-05-19 00:56:29 +0530

    Add .gitignore and optimize message conversations query

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -62,38 +62,48 @@ export const getMessages = async (req, res) => {
 // Get all conversations (unique users with messages)
 export const getConversations = async (req, res) => {
   try {
-    // Get all users that current user has messaged with
-    const messages = await Message.find({
-      $or: [{ sender: req.user._id }, { receiver: req.user._id }],
-    }).sort({ createdAt: -1 });
+    // Get all unique conversations
+    const conversations = await Message.aggregate([
+      {
+        $match: {
+          $or: [
+            { sender: req.user._id },
+            { receiver: req.user._id }
+          ]
+        }
+      },
+      {
+        $group: {
+          _id: {
+            $cond: [
+              { $eq: ['$sender', req.user._id] },
+              '$receiver',
+              '$sender'
+            ]
+          },
+          lastMessage: { $last: '$content' },
+          lastMessageTime: { $last: '$createdAt' },
+        }
+      },
+      { $sort: { lastMessageTime: -1 } }
+    ]);
 
-    // Get unique users
-    const userMap = new Map();
-    messages.forEach((msg) => {
-      const otherUserId = msg.sender._id.toString() === req.user._id.toString() ? msg.receiver : msg.sender;
-      if (!userMap.has(otherUserId.toString())) {
-        userMap.set(otherUserId.toString(), {
-          userId: otherUserId,
-          lastMessage: msg.content,
-          lastMessageTime: msg.createdAt,
+    // Populate user details
+    const result = [];
+    for (const conv of conversations) {
+      const user = await User.findById(conv._id).select('_id name profilePicture email');
+      if (user) {
+        result.push({
+          user,
+          lastMessage: conv.lastMessage,
+          lastMessageTime: conv.lastMessageTime,
         });
       }
-    });
-
-    // Get user details for each conversation
-    const conversations = [];
-    for (const [, conv] of userMap) {
-      const user = await User.findById(conv.userId).select('_id name profilePicture email');
-      conversations.push({
-        user,
-        lastMessage: conv.lastMessage,
-        lastMessageTime: conv.lastMessageTime,
-      });
     }
 
     res.status(200).json({
       success: true,
-      data: conversations.sort((a, b) => new Date(b.lastMessageTime) - new Date(a.lastMessageTime)),
+      data: result,
     });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
